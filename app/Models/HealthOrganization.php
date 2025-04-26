21985a5a1230056b1007938452135eb38ff68aef
= <=>
2025-04-26 10:07:34 +0800

    add HealthOrganization Model

--- app/Models/HealthOrganization.php
+++ app/Models/HealthOrganization.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class HealthOrganization extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'created_by',
+        'updated_by',
+    ];
+
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function updator()
+    {
+        return $this->belongsTo(User::class, 'updated_by');
+    }
+    
 }
