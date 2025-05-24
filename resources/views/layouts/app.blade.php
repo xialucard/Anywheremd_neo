2fbccf4fae32fcb60217f646b327cccb4e0b2709
= <=>
2025-05-24 12:38:31 +0800

    Fix minor defects. Add new logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -94,9 +94,9 @@
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     @if(!empty(Auth::user()->user_type) && Auth::user()->user_type == "Doctor")
-                    <img src="{{ asset('storage/uploads/img/Anywheremd_logo.png') }}" width="200px">
+                    <img src="{{ asset('img/AnywhereMD_logo.png') }}" width="200px">
                     @else
-                    <img src="{{ asset('storage/uploads/img/Anywheremd_white_logo.png') }}" width="200px">
+                    <img src="{{ asset('img/AnywhereMD_white_logo.png') }}" width="200px">
                     @endif
                     {{-- {{ config('app.name', 'Anywhere MD') }} {{ !empty(Auth::user()->user_type) ? '(' . Auth::user()->user_type . ')' : '' }} --}}
                 </a>
