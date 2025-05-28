e98046c8e1ca24204eeed7d36c139bc0fac65770
= <=>
2025-05-29 00:02:21 +0800

    Fix RX pic in e-prescription

--- app/Http/Controllers/DoctorsHomeController.php
+++ app/Http/Controllers/DoctorsHomeController.php
@@ -516,7 +516,7 @@ class DoctorsHomeController extends Controller
     }
 
     function pdfPrescription(Consultation $doctors_home){
-        $pdf = Pdf::setOptions(['defaultFont' => 'sans-serif', 'chroot' => [public_path('storage/rx.jpg'), public_path('storage/' . $doctors_home->doctor->sig_pic)]])->loadView($this->viewFolder . '.pdfPrescription', ['datum' => $doctors_home]);
+        $pdf = Pdf::setOptions(['defaultFont' => 'sans-serif', 'chroot' => public_path('storage/' . $doctors_home->doctor->sig_pic)])->loadView($this->viewFolder . '.pdfPrescription', ['datum' => $doctors_home]);
         // $pdf->getDomPDF()->setHttpContext(
         //     stream_context_create([
         //         'ssl' => [
