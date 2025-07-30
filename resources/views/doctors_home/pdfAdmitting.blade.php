fa824d66fc490c04c81715eec31f74ed9e90976f
= <=>
2025-07-30 19:10:09 +0800

    Fix Signature

--- resources/views/doctors_home/pdfAdmitting.blade.php
+++ resources/views/doctors_home/pdfAdmitting.blade.php
@@ -85,7 +85,9 @@
     <br>
     <br>
     <div class="position-absolute top-100 start-100 text-end mt-5">
-        <img src="{{ public_path('storage/' . $datum->doctor->sig_pic) }}" style="width:1in"><br>
+        @if($datum->doctor->sig_pic != "" || $referal_conso->doctor->sig_pic != "")
+        <img src="{{ public_path('storage/' . isset($referal_conso) ? $referal_conso->doctor->sig_pic : $datum->doctor->sig_pic) }}" style="width:1in"><br>
+        @endif
         {{ str_pad("", strlen(isset($referal_conso) ? $referal_conso->doctor->name : $datum->doctor->name), "_", STR_PAD_LEFT) }}<br>
         Dr. {{ isset($referal_conso) ? $referal_conso->doctor->name : $datum->doctor->name }}<br>
         PRC#: {{ $datum->doctor->prc_number }}
--- resources/views/doctors_home/pdfMedCert.blade.php
+++ resources/views/doctors_home/pdfMedCert.blade.php
@@ -116,7 +116,9 @@
         I certify that this information is generated from the Electronic Medical Records system in my clinic and by generating this form, my signature is hereby affixed.
     </p>
     <div class="position-absolute top-100 start-100 text-end mt-5">
+        @if($datum->doctor->sig_pic != "")
         <img src="{{ public_path('storage/' . $datum->doctor->sig_pic) }}" style="width:1in"><br>
+        @endif
         {{ str_pad("", strlen(isset($referal_conso) ? $referal_conso->doctor->name : $datum->doctor->name), "_", STR_PAD_LEFT) }}<br>
         Dr. {{ isset($referal_conso) ? $referal_conso->doctor->name : $datum->doctor->name }}<br>
         PRC#: {{ isset($referal_conso) ? $referal_conso->doctor->prc_number : $datum->doctor->prc_number }}
