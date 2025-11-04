2dcb63d96f819ec3d4167487936fc33c0ac55b30
= <=>
2025-11-05 07:50:51 +0800

    Add SOAP to Dialysis Clinic Home booking

--- resources/views/patient_records/inputForm.blade.php
+++ resources/views/patient_records/inputForm.blade.php
@@ -889,7 +889,7 @@
             @if(isset($bookings[0]->booking_type) && $bookings[0]->booking_type == 'Dialysis')
             @can('clinics_home.pdfHD')
             {{-- <button type="button" class="btn btn-{{ $bgColor }} modalForm-close" data-bs-dismiss="modal">Close</button> --}}
-            <div class="m-1"><a class="btn btn-{{ $bgColor }} btn-sm w-100" href="{{ route('clinics_home.pdfHD', [isset($referal_conso) ? $referal_conso->id : $dat->id, !empty(parse_url(Request::fullUrl())['query']) ? parse_url(Request::fullUrl())['query'] : '']) }}" title="Print HD Form" role="button" download><i class="bi bi-filetype-pdf"></i><span class="ps-1 d-sm-none">Print HD Form</span></a></div>
+            <div class="m-1"><a class="btn btn-{{ $bgColor }} btn-sm w-100" href="{{ route('clinics_home.pdfHD', [isset($referal_conso) ? $referal_conso->id : $bookings[0]->id, !empty(parse_url(Request::fullUrl())['query']) ? parse_url(Request::fullUrl())['query'] : '']) }}" title="Print HD Form" role="button" download><i class="bi bi-filetype-pdf"></i><span class="ps-1 d-sm-none">Print HD Form</span></a></div>
             @endcan
             <div class="row mt-3">
               <div class="col-lg-4">
@@ -2965,7 +2965,7 @@
             </div>
             @can('clinics_home.pdfHD')
             {{-- <button type="button" class="btn btn-{{ $bgColor }} modalForm-close" data-bs-dismiss="modal">Close</button> --}}
-            <div class="m-1"><a class="btn btn-{{ $bgColor }} btn-sm w-100" href="{{ route('clinics_home.pdfHD', [isset($referal_conso) ? $referal_conso->id : $dat->id, !empty(parse_url(Request::fullUrl())['query']) ? parse_url(Request::fullUrl())['query'] : '']) }}" title="Print HD Form" role="button" download><i class="bi bi-filetype-pdf"></i><span class="ps-1 d-sm-none">Print HD Form</span></a></div>
+            <div class="m-1"><a class="btn btn-{{ $bgColor }} btn-sm w-100" href="{{ route('clinics_home.pdfHD', [isset($referal_conso) ? $referal_conso->id : $bookings[0]->id, !empty(parse_url(Request::fullUrl())['query']) ? parse_url(Request::fullUrl())['query'] : '']) }}" title="Print HD Form" role="button" download><i class="bi bi-filetype-pdf"></i><span class="ps-1 d-sm-none">Print HD Form</span></a></div>
             @endcan
             @endif
           </div>
